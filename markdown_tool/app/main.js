92629c4e3cf64bdd41f86c8d2634d40a3114d186
ZoeLiao <zoejoyuliao@gmail.com>
2019-03-24 23:49:08 +0800

    Open file from the operating system and render it in the html

--- markdown_tool/app/main.js
+++ markdown_tool/app/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
 // 1. Declare here to avoid being collected by GC
 // 2. The 'let' statement declares a block scope local variable,
@@ -9,15 +10,39 @@ var currentPath = process.cwd();
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ show: false });
   // Debug
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.webContents.loadFile(currentPath + '/app/index.html');
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
+    // Call getFileFromUser when the window is ready to show
+    getFileFromUser();
   });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
+
+const getFileFromUser = exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt'] },
+      { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+    ]
+  });
+
+  if (!files) {
+    return;
+  }
+
+  const file = files[0];
+  const openFile = (file) => {
+    // fs.readFileSync() return a buffer object
+    const content = fs.readFileSync(file).toString();
+    mainWindow.webContents.send('file-opened', file, content);
+  };
+
+}
